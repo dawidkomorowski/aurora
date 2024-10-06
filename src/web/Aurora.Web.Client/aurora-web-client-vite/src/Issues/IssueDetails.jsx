6c319e7ba49e5fd6df1432c477c7e4b784ee9fd7
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-10-06 15:08:55 +0200

    Refactor IssueDetails into IssueDetailsView, add edit mode

--- src/web/Aurora.Web.Client/aurora-web-client-vite/src/Issues/IssueDetails.jsx
+++ src/web/Aurora.Web.Client/aurora-web-client-vite/src/Issues/IssueDetails.jsx
@@ -1,34 +1,18 @@
-import { useState, useEffect } from "react";
-import { IssuesServiceClient } from "./IssuesServiceClient";
-import { useParams } from "react-router-dom";
-
-
-export function IssueDetails() {
-    const { issueId } = useParams();
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        IssuesServiceClient.get(issueId).then(responseData => {
-            setData(responseData);
-        }).catch(error => {
-            console.error(error);
-        });
-    }, []);
-
+export function IssueDetails({ title, description, status }) {
     return (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-            <div style={{ width: "50%", backgroundColor: "lightgray", padding: "10px" }} >
-                <div><strong>Id: </strong>{data.id}</div>
-                <div><h1>{data.title}</h1></div>
-                <div><h4>Description</h4></div>
-                <div>{data.description}</div>
-                <br />
-                <div><strong>Status: </strong>{data.status}</div>
-                <br />
-                <div><strong>Created: </strong>{data.createdDateTime}</div>
-                <div><strong>Updated: </strong>{data.updatedDateTime}</div>
-            </div>
-        </div >
+        <div>
+            <div><h1>{title}</h1></div>
+            <div><h4>Description</h4></div>
+            <div>{description}</div>
+            <br />
+            <div><strong>Status: </strong>{status}</div>
+        </div>
+    );
+}
+
+export function IssueEditor({ data, setData }) {
+    return (
+        <div></div>
     );
 }
 
---
+++ src/web/Aurora.Web.Client/aurora-web-client-vite/src/Issues/IssueDetailsView.jsx
@@ -0,0 +1,52 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { IssuesServiceClient } from "./IssuesServiceClient";
+import { IssueDetails, IssueEditor } from "./IssueDetails";
+
+export function IssueDetailsView() {
+    const { issueId } = useParams();
+    const [editMode, setEditMode] = useState(false);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        IssuesServiceClient.get(issueId).then(responseData => {
+            setData(responseData);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, []);
+
+    function handleEditButtonClick() {
+        setEditMode(true);
+    }
+
+    function handleSaveButtonClick() {
+        setEditMode(false);
+    }
+
+    let content;
+    let button;
+    if (editMode) {
+        content = <IssueEditor data={data} setData={setData} />
+        button = <button onClick={handleSaveButtonClick}>Save</button>
+    }
+    else {
+        content = <IssueDetails title={data.title} description={data.description} status={data.status} />
+        button = <button onClick={handleEditButtonClick}>Edit</button>
+    }
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ width: "50%", backgroundColor: "lightgray", padding: "10px" }} >
+                <div><strong>Id: </strong>{data.id}</div>
+                {content}
+                <br />
+                <div><strong>Created: </strong>{data.createdDateTime}</div>
+                <div><strong>Updated: </strong>{data.updatedDateTime}</div>
+                <div style={{ marginTop: "20px", float: "right" }}>
+                    {button}
+                </div>
+            </div>
+        </div >
+    );
+}

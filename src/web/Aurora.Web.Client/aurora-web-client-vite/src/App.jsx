db814f1c454d4b2f6a6b520032b84c8fe17f6b22
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-10-06 12:11:18 +0200

    By default redirect to Issue Explorer

--- src/web/Aurora.Web.Client/aurora-web-client-vite/src/App.jsx
+++ src/web/Aurora.Web.Client/aurora-web-client-vite/src/App.jsx
@@ -1,6 +1,6 @@
 import { IssueExplorer } from "./Issues/IssueExplorer"
 import { IssueDetails } from "./Issues/IssueDetails"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { RootLayout } from "./RootLayout/RootLayout"
 import { NotFoundView } from "./RootLayout/NotFoundView"
 
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootLayout />}>
+          <Route index element={<Navigate to="issue-explorer" replace />} />
           <Route path="issue-explorer" element={<IssueExplorer />} />
           <Route path="issue/:issueId" element={<IssueDetails />} />
           <Route path="*" element={<NotFoundView />} />

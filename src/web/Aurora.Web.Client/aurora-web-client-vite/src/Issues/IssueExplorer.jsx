4b4e535c3a49c825b886feb35cab4cd1d99316e7
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-10-07 21:22:49 +0200

    Add status filter to Issue Explorer view

--- src/web/Aurora.Web.Client/aurora-web-client-vite/src/Issues/IssueExplorer.jsx
+++ src/web/Aurora.Web.Client/aurora-web-client-vite/src/Issues/IssueExplorer.jsx
@@ -3,20 +3,40 @@ import { IssueList } from "./IssueList";
 import { IssuesServiceClient } from "./IssuesServiceClient"
 
 export function IssueExplorer() {
+    const [statusFilter, setStatusFilter] = useState("");
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        IssuesServiceClient.getAll().then(responseData => {
+        const filters = {
+            status: statusFilter || null
+        };
+
+        IssuesServiceClient.getAll(filters).then(responseData => {
             setData(responseData);
         }).catch(error => {
             console.error(error)
         });
-    }, []);
+    }, [statusFilter]);
+
+    function handleStatusFilterChange(event) {
+        setStatusFilter(event.target.value);
+    }
 
     return (
-        <>
+        <div>
+            <div style={{ display: "flex" }}>
+                <div style={{ margin: "10px" }}>
+                    <strong>Status</strong>
+                </div>
+                <select value={statusFilter} onChange={handleStatusFilterChange} style={{ width: "200px" }}>
+                    <option value="">All</option>
+                    <option value="Open">Open</option>
+                    <option value="In Progress">In Progress</option>
+                    <option value="Closed">Closed</option>
+                </select>
+            </div>
             <IssueList data={data} />
-        </>
+        </div>
     );
 }
 

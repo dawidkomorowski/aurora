2c225cd2522f862e9aa74a7ab0d54f98587597f1
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-10-31 17:38:04 +0100

    Implement basic Get and Create methods for version controller

--- src/services/issues-service/Aurora.IssuesService.Host/Controllers/VersionController.cs
+++ src/services/issues-service/Aurora.IssuesService.Host/Controllers/VersionController.cs
@@ -1,4 +1,7 @@
-﻿using Aurora.IssuesService.DataStore;
+﻿using System.ComponentModel.DataAnnotations;
+using Aurora.IssuesService.DataStore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aurora.IssuesService.Host.Controllers;
@@ -10,6 +13,23 @@ namespace Aurora.IssuesService.Host.Controllers;
 // TODO - Version must be non white space only string.
 // TODO - Version string must be trimmed from white spaces.
 
+public sealed class VersionDetailsResponse
+{
+    public required int Id { get; init; }
+    public required string Name { get; init; }
+}
+
+public sealed class CreateVersionRequest
+{
+    [Required(AllowEmptyStrings = false)]
+    public string Name { get; set; } = string.Empty;
+}
+
+public sealed class CreateVersionResponse
+{
+    public required int Id { get; init; }
+}
+
 [ApiController]
 [Route("api/versions")]
 public sealed class VersionController : ControllerBase
@@ -19,5 +39,39 @@ public sealed class VersionController : ControllerBase
     public VersionController(IIssuesStorage issuesStorage)
     {
         _issuesStorage = issuesStorage;
+    }
+
+    [HttpGet("{id:int}")]
+    public Results<NotFound, Ok<VersionDetailsResponse>> Get(int id)
+    {
+        try
+        {
+            var versionReadDto = _issuesStorage.GetVersion(id);
+            var versionDetailsResponse = new VersionDetailsResponse
+            {
+                Id = versionReadDto.Id,
+                Name = versionReadDto.Name
+            };
+            return TypedResults.Ok(versionDetailsResponse);
+        }
+        catch (VersionNotFoundException)
+        {
+            return TypedResults.NotFound();
+        }
+    }
+
+    [HttpPost]
+    public Results<BadRequest, Created<CreateVersionResponse>> Create(CreateVersionRequest createVersionRequest)
+    {
+        var versionCreateDto = new VersionCreateDto
+        {
+            Name = createVersionRequest.Name
+        };
+
+        var versionReadDto = _issuesStorage.CreateVersion(versionCreateDto);
+
+        var uri = Url.Action(nameof(Get), new { id = versionReadDto.Id });
+        var createVersionResponse = new CreateVersionResponse { Id = versionReadDto.Id };
+        return TypedResults.Created(uri, createVersionResponse);
     }
 }

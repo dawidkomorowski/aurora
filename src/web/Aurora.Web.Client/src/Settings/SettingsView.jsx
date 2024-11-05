deed65f0537afefb3a58e7e6f1e8386bc8e75762
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-11-05 22:39:21 +0100

    Add basic read-only list of static versions

--- src/web/Aurora.Web.Client/src/Settings/SettingsView.jsx
+++ src/web/Aurora.Web.Client/src/Settings/SettingsView.jsx
@@ -1,7 +1,39 @@
 export function SettingsView() {
+    const versions = [
+        {
+            id: 1,
+            name: "Version 1"
+        },
+        {
+            id: 2,
+            name: "Version 2"
+        },
+        {
+            id: 3,
+            name: "Version 3"
+        }
+    ];
+
+    var versionItems = versions.map(v => {
+        return (
+            <div>{v.name}</div>
+        );
+    });
+
     return (
-        <>
-            <span>Settings View</span>
-        </>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ width: "50%", backgroundColor: "lightgray", padding: "10px" }} >
+                <span>
+                    <h1>Settings</h1>
+                </span>
+                <br />
+                <span>
+                    <h2>Versions</h2>
+                </span>
+                <div>
+                    {versionItems}
+                </div>
+            </div>
+        </div >
     );
 }

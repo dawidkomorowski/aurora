8f53693b400ceaf15e95d5e4f1a7482151514b63
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-11-12 01:41:20 +0100

    Create VersionFilter component and place it in issue explorer

--- src/web/Aurora.Web.Client/src/Issues/Explorer/IssueExplorer.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Explorer/IssueExplorer.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { IssueList } from "./IssueList";
 import { IssueApiClient } from "../../ApiClients/IssueApiClient";
+import { ShowAllVersionFilter, VersionFilter } from "./VersionFilter";
 
 export function IssueExplorer() {
     const [statusFilter, setStatusFilter] = useState("");
+    const [versionFilter, setVersionFilter] = useState(ShowAllVersionFilter);
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -16,7 +18,7 @@ export function IssueExplorer() {
         }).catch(error => {
             console.error(error)
         });
-    }, [statusFilter]);
+    }, [statusFilter, versionFilter]);
 
     function handleStatusFilterInput(event) {
         setStatusFilter(event.target.value);
@@ -25,15 +27,23 @@ export function IssueExplorer() {
     return (
         <div>
             <div style={{ display: "flex" }}>
-                <div style={{ margin: "10px" }}>
-                    <strong>Status</strong>
+                <div style={{ display: "flex" }}>
+                    <div style={{ margin: "10px" }}>
+                        <strong>Status</strong>
+                    </div>
+                    <select value={statusFilter} onInput={handleStatusFilterInput} style={{ width: "200px" }}>
+                        <option value="">All</option>
+                        <option value="Open">Open</option>
+                        <option value="In Progress">In Progress</option>
+                        <option value="Closed">Closed</option>
+                    </select>
                 </div>
-                <select value={statusFilter} onInput={handleStatusFilterInput} style={{ width: "200px" }}>
-                    <option value="">All</option>
-                    <option value="Open">Open</option>
-                    <option value="In Progress">In Progress</option>
-                    <option value="Closed">Closed</option>
-                </select>
+                <div style={{ display: "flex" }}>
+                    <div style={{ margin: "10px" }}>
+                        <strong>Version</strong>
+                    </div>
+                    <VersionFilter versionFilter={versionFilter} setVersionFilter={setVersionFilter} />
+                </div>
             </div>
             <IssueList data={data} />
         </div>

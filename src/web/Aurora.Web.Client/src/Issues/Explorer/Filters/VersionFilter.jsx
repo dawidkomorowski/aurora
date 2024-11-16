fa6ae827930d43d3f4e5a66b325b391b7af0a625
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-11-16 16:43:42 +0100

    Fallback to default value when value from search filters is not valid

--- src/web/Aurora.Web.Client/src/Issues/Explorer/Filters/VersionFilter.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Explorer/Filters/VersionFilter.jsx
@@ -2,9 +2,6 @@ import { useEffect, useState } from "react";
 import { VersionApiClient } from "../../../ApiClients/VersionApiClient";
 import { useSearchFilters } from "./useSearchFilters";
 
-// TODO When value from search params is not available in filter it should be set to some default value i.e. nonexistent version ID 123 is found in params but such version ID is not available in filters.
-// TODO Challenge is in fact that once the page is refreshed and before fetching filter options it will always fallback to default value.
-
 export function VersionFilter() {
     const [searchFilters, setSearchFilters] = useSearchFilters();
     const [version, setVersion] = useState(ShowAllVersionFilter);
@@ -48,6 +45,15 @@ export function VersionFilter() {
 
         const versionFromSearchFilters = versions.find(v => v.id === (searchFilters.versionId ?? -1)) ?? ShowAllVersionFilter;
         setVersion(versionFromSearchFilters);
+
+        if (versionFromSearchFilters === ShowAllVersionFilter) {
+            const newSearchFilters = {
+                ...searchFilters,
+                versionId: null
+            };
+
+            setSearchFilters(newSearchFilters);
+        }
     }, [searchFilters, versions]);
 
     function handleInput(event) {

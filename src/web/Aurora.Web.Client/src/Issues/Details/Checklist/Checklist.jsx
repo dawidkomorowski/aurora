67c9514c7adbb43ad8d00c7c8a84e7032ddd369b
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-11-19 23:53:28 +0100

    Extend mock data

--- src/web/Aurora.Web.Client/src/Issues/Details/Checklist/Checklist.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Details/Checklist/Checklist.jsx
@@ -1,18 +1,13 @@
 import { ChecklistItem } from "./ChecklistItem";
 
-export function Checklist({ title }) {
-    const data = {
-        ...mockData,
-        title: title
-    };
-
-    const items = data.items.map(i => <ChecklistItem key={i.id} id={i.id} content={i.content} checked={i.checked} />);
+export function Checklist({ checklistData }) {
+    const items = checklistData.items.map(i => <ChecklistItem key={i.id} id={i.id} content={i.content} checked={i.checked} />);
 
     return (
         <div style={{ borderStyle: "solid", borderWidth: "2px", padding: "10px" }}>
             <div style={{ display: "flex" }}>
                 <div>
-                    <strong>{data.title}</strong>
+                    <strong>{checklistData.title}</strong>
                 </div>
                 <div style={{ marginLeft: "auto" }}>
                     <button>Add item</button>
@@ -27,24 +22,3 @@ export function Checklist({ title }) {
     );
 }
 
-const mockData = {
-    id: 1,
-    title: "Checklist 1",
-    items: [
-        {
-            id: 1,
-            content: "First item is unchecked.",
-            checked: false
-        },
-        {
-            id: 2,
-            content: "Second item is checked.",
-            checked: true
-        },
-        {
-            id: 3,
-            content: "Third item is also unchecked.",
-            checked: false
-        }
-    ]
-}
--- src/web/Aurora.Web.Client/src/Issues/Details/Checklist/ChecklistsSection.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Details/Checklist/ChecklistsSection.jsx
@@ -8,12 +8,67 @@ export function ChecklistsSection() {
                     <strong>Checklists</strong>
                 </div>
                 <div style={{ marginTop: "10px" }}>
-                    <Checklist title="Checklist 1" />
+                    <Checklist checklistData={mockData[0]} />
                 </div>
                 <div style={{ marginTop: "10px" }}>
-                    <Checklist title="Checklist 2" />
+                    <Checklist checklistData={mockData[1]} />
                 </div>
             </div>
         </div >
     );
 }
+
+const mockData = [
+    {
+        id: 1,
+        title: "Checklist 1",
+        items: [
+            {
+                id: 1,
+                content: "First item is unchecked.",
+                checked: false
+            },
+            {
+                id: 2,
+                content: "Second item is checked.",
+                checked: true
+            },
+            {
+                id: 3,
+                content: "Third item is also unchecked.",
+                checked: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Acceptance criteria",
+        items: [
+            {
+                id: 4,
+                content: "Implement feature.",
+                checked: true
+            },
+            {
+                id: 5,
+                content: "Add unit tests.",
+                checked: true
+            },
+            {
+                id: 6,
+                content: "Merge to main branch.",
+                checked: false
+            },
+            {
+                id: 7,
+                content: "Manual testing.",
+                checked: false
+            },
+            {
+                id: 8,
+                content: "Deploy to DEV.",
+                checked: false
+            }
+        ]
+    }
+]

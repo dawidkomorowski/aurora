ec8800be74e84f3e5adb92713782ce1a7ee1a492
Dawid Komorowski <dawidkomorowski@outlook.com>
2024-12-18 17:09:21 +0100

    Add edit mode to checklist

--- src/web/Aurora.Web.Client/src/Issues/Details/Checklist/Checklist.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Details/Checklist/Checklist.jsx
@@ -1,22 +1,68 @@
+import { useState } from "react";
 import { ChecklistItem } from "./ChecklistItem";
 
 export function Checklist({ checklist, onRemove }) {
+    const [editMode, setEditMode] = useState(false);
+
     const items = checklist.items.map(i => <ChecklistItem key={i.id} id={i.id} content={i.content} isChecked={i.isChecked} />);
+
+    function handleEditButtonClick() {
+        setEditMode(true);
+    }
+
+    function handleSaveButtonClick() {
+        setEditMode(false);
+    }
+
+    function handleCancelButtonClick() {
+        setEditMode(false);
+    }
 
     function handleRemoveButtonClick() {
         onRemove(checklist.id);
     }
 
+    let content;
+    let buttons;
+
+    if (editMode) {
+        content = (
+            <>
+                <input type="text" style={{ width: "100%", boxSizing: "border-box" }} />
+            </>
+        );
+
+        buttons = (
+            <>
+                <button onClick={handleSaveButtonClick}>Save</button>
+                <button onClick={handleCancelButtonClick} style={{ marginLeft: "5px" }}>Cancel</button>
+            </>
+        );
+    }
+    else {
+        content = (
+            <>
+                <strong>{checklist.title}</strong>
+            </>
+        );
+
+        buttons = (
+            <>
+                <button>Add item</button>
+                <button onClick={handleEditButtonClick} style={{ marginLeft: "5px" }}>Edit</button>
+                <button onClick={handleRemoveButtonClick} style={{ marginLeft: "5px" }}>Remove</button>
+            </>
+        );
+    }
+
     return (
         <div style={{ borderStyle: "solid", borderWidth: "2px", padding: "10px" }}>
             <div style={{ display: "flex" }}>
-                <div>
-                    <strong>{checklist.title}</strong>
+                <div style={{ flexGrow: "1", marginRight: "10px" }}>
+                    {content}
                 </div>
                 <div style={{ marginLeft: "auto" }}>
-                    <button>Add item</button>
-                    <button style={{ marginLeft: "5px" }}>Edit</button>
-                    <button onClick={handleRemoveButtonClick} style={{ marginLeft: "5px" }}>Remove</button>
+                    {buttons}
                 </div>
             </div>
             <div>

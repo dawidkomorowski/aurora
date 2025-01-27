43a9461c14e367873a41399a138e0c3606161bd2
Dawid Komorowski <dawidkomorowski@outlook.com>
2025-01-27 22:12:42 +0100

    Add switching to edit mode of checklist item

--- src/web/Aurora.Web.Client/src/Issues/Details/Checklist/ChecklistItem.jsx
+++ src/web/Aurora.Web.Client/src/Issues/Details/Checklist/ChecklistItem.jsx
@@ -1,23 +1,75 @@
+import { useState } from "react";
 import styles from "./ChecklistItem.module.css"
 
 export function ChecklistItem({ id, content, isChecked, onRemove }) {
+    const [editMode, setEditMode] = useState(false);
 
-    function handleInput() {
+    function handleIsCheckedInput() {
+    }
+
+    function handleContentInput() {
+
+    }
+
+    function handleEditButtonClick() {
+        setEditMode(true);
     }
 
     function handleRemoveButtonClick() {
         onRemove(id);
     }
 
+    function handleSaveButtonClick() {
+    }
+
+    function handleCancelButtonClick() {
+        setEditMode(false);
+    }
+
+    let contentElement;
+    let buttons;
+
+    if (editMode) {
+        contentElement = (
+            <>
+                <input type="text" value={content} onInput={handleContentInput} style={{ width: "100%", boxSizing: "border-box" }} />
+            </>
+        );
+
+        buttons = (
+            <>
+                <button onClick={handleSaveButtonClick}>Save</button>
+                <button onClick={handleCancelButtonClick} style={{ marginLeft: "5px" }}>Cancel</button>
+            </>
+        );
+    }
+    else {
+        contentElement = (
+            <>
+                <div style={{ display: "inline-flex" }}>
+                    <div>
+                        <input type="checkbox" checked={isChecked} onChange={handleIsCheckedInput} />
+                    </div>
+                    <div>{content}</div>
+                </div>
+            </>
+        );
+
+        buttons = (
+            <>
+                <button onClick={handleEditButtonClick}>Edit</button>
+                <button onClick={handleRemoveButtonClick} style={{ marginLeft: "5px" }}>Remove</button>
+            </>
+        );
+    }
+
     return (
         <div className={styles.checklistItem} style={{ display: "flex", margin: "5px 0px 5px 0px" }}>
-            <div>
-                <input type="checkbox" checked={isChecked} onChange={handleInput} />
+            <div style={{ flexGrow: "1", marginRight: "10px" }}>
+                {contentElement}
             </div>
-            <div>{content}</div>
             <div style={{ marginLeft: "auto" }}>
-                <button>Edit</button>
-                <button onClick={handleRemoveButtonClick} style={{ marginLeft: "5px" }}>Remove</button>
+                {buttons}
             </div>
         </div>
     );
